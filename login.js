ff806fa721e37dd61c3e087ea1e8d5511b923ed3
Harshsinh <harshgaming0958@gmail.com>
2025-01-15 22:44:20 +0530

    logi

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
 
     if (username === validUsername && password === validPassword) {
         localStorage.setItem('loggedIn', 'true'); // Store login state
-        window.location.href = 'index.html'; // Redirect to the main page
+        window.location.href = 'main.html'; // Redirect to the main page
     } else {
         alert('Invalid username or password. Please try again.'); // Show error message
     }
